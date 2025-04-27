bfe45a458330b1c3661d4751cadd4b8194cb7a4d
saemii-24 <hims2419@gmail.com>
2025-04-27 15:01:44 +0900

    fix: server 에러
    - docker error 발생
    - table 삭제 되어 재생성

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
+from . import models
+from .database import engine
 from .routers import todo, login
+
+models.Base.metadata.create_all(bind=engine) 
 
 app = FastAPI()
 
